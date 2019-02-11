d16d5312e1d21191bcb6b27785239b914eb01169
panu0 <panu.linnal4@gmail.com>
2019-02-11 02:14:34 +0200

    remove debugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,12 +45,7 @@ module.exports = {
         {
             resolve: `gatsby-plugin-purgecss`,
             options: {
-                // develop: true, // Enable while using `gatsby develop`
                 tailwind: true,
-                debug: true,
-                // whitelist: ['whitelist'], // Don't remove this selector
-                // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-                //purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
                 purgeOnly: ["src/styles/index.css"]
             }
         },
